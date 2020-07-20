63f54f58aa79f354bc9567b6bdd2c20d50590a2a
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-07-20 10:36:45 +0200

    Fix setting timezones on test data
    Setting tzinfo with pytz offsets produces unreliable offsets.
    `.localize()` should be used instead

--- src/test_main.py
+++ src/test_main.py
@@ -15,25 +15,25 @@ def test_earlier_timezone():
 
 
 @pytest.mark.parametrize('submission_date, current_timezone, expected_validity', [
-    (dt.datetime(2020, 1, 1, 12, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),  # Wednesday
-    (dt.datetime(2020, 1, 1, 9, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),
-    (dt.datetime(2020, 1, 1, 9, tzinfo=CURRENT_TIMEZONE), UTC, False),
-    (dt.datetime(2020, 1, 1, 10, tzinfo=CURRENT_TIMEZONE), UTC, True),
-    (dt.datetime(2020, 1, 1, 8, 59, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),
-    (dt.datetime(2020, 1, 1, 17, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, False),
-    (dt.datetime(2020, 1, 1, 17, tzinfo=CURRENT_TIMEZONE), UTC, True),
-    (dt.datetime(2020, 1, 1, 16, 59, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),
-    (dt.datetime(2020, 1, 2, 12, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),  # Thursday
-    (dt.datetime(2020, 1, 3, 12, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),  # Friday
-    (dt.datetime(2020, 1, 4, 12, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, False),  # Saturday
-    (dt.datetime(2020, 1, 5, 12, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, False),  # Sunday
-    (dt.datetime(2020, 1, 6, 12, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),  # Monday
-    (dt.datetime(2020, 1, 7, 12, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),  # Tuesday
-    (dt.datetime(2020, 6, 15, 9, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),  # DST
-    (dt.datetime(2020, 6, 15, 10, tzinfo=CURRENT_TIMEZONE), CURRENT_TIMEZONE, True),  # DST
-    (dt.datetime(2020, 6, 15, 10, tzinfo=CURRENT_TIMEZONE), UTC, False),
-    (dt.datetime(2020, 6, 15, 11, tzinfo=CURRENT_TIMEZONE), UTC, True),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 12)), CURRENT_TIMEZONE, True),  # Wednesday
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 9)), CURRENT_TIMEZONE, True),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 9)), UTC, False),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 10)), UTC, True),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 8, 59)), CURRENT_TIMEZONE, False),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 17)), CURRENT_TIMEZONE, False),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 17)), UTC, True),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 16, 59)), CURRENT_TIMEZONE, True),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 2, 12)), CURRENT_TIMEZONE, True),  # Thursday
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 3, 12)), CURRENT_TIMEZONE, True),  # Friday
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 4, 12)), CURRENT_TIMEZONE, False),  # Saturday
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 5, 12)), CURRENT_TIMEZONE, False),  # Sunday
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 6, 12)), CURRENT_TIMEZONE, True),  # Monday
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 7, 12)), CURRENT_TIMEZONE, True),  # Tuesday
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 9)), CURRENT_TIMEZONE, True),  # DST
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 10)), CURRENT_TIMEZONE, True),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 10)), UTC, False),
+    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 11)), UTC, True),
 ])
 def test_validate_submission_date(submission_date, current_timezone, expected_validity):
     is_valid, reason = validate_submission_date(submission_date, current_timezone)
-    assert is_valid is expected_validity
+    assert is_valid is expected_validity, reason
