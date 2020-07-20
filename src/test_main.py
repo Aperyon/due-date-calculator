535030c0cbe32b14aa7abe504f243e945121940e
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-07-20 10:47:40 +0200

    Reformat submission date validator tests

--- src/test_main.py
+++ src/test_main.py
@@ -14,26 +14,29 @@ def test_earlier_timezone():
     assert UTC.localize(now) > CURRENT_TIMEZONE.localize(now)
 
 
-@pytest.mark.parametrize('submission_date, current_timezone, expected_validity', [
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 12)), CURRENT_TIMEZONE, True),  # Wednesday
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 9)), CURRENT_TIMEZONE, True),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 9)), UTC, False),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 10)), UTC, True),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 8, 59)), CURRENT_TIMEZONE, False),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 17)), CURRENT_TIMEZONE, False),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 17)), UTC, True),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 16, 59)), CURRENT_TIMEZONE, True),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 2, 12)), CURRENT_TIMEZONE, True),  # Thursday
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 3, 12)), CURRENT_TIMEZONE, True),  # Friday
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 4, 12)), CURRENT_TIMEZONE, False),  # Saturday
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 5, 12)), CURRENT_TIMEZONE, False),  # Sunday
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 6, 12)), CURRENT_TIMEZONE, True),  # Monday
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 7, 12)), CURRENT_TIMEZONE, True),  # Tuesday
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 9)), CURRENT_TIMEZONE, True),  # DST
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 10)), CURRENT_TIMEZONE, True),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 10)), UTC, False),
-    (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 11)), UTC, True),
-])
+@pytest.mark.parametrize(
+    "submission_date, current_timezone, expected_validity",
+    [
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 12)), CURRENT_TIMEZONE, True),  # Wednesday
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 9)), CURRENT_TIMEZONE, True),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 9)), UTC, False),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 10)), UTC, True),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 8, 59)), CURRENT_TIMEZONE, False),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 17)), CURRENT_TIMEZONE, False),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 17)), UTC, True),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 1, 16, 59)), CURRENT_TIMEZONE, True),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 2, 12)), CURRENT_TIMEZONE, True),  # Thursday
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 3, 12)), CURRENT_TIMEZONE, True),  # Friday
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 4, 12)), CURRENT_TIMEZONE, False),  # Saturday
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 5, 12)), CURRENT_TIMEZONE, False),  # Sunday
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 6, 12)), CURRENT_TIMEZONE, True),  # Monday
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 1, 7, 12)), CURRENT_TIMEZONE, True),  # Tuesday
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 9)), CURRENT_TIMEZONE, True),  # DST
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 10)), CURRENT_TIMEZONE, True),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 10)), UTC, False),
+        (CURRENT_TIMEZONE.localize(dt.datetime(2020, 6, 15, 11)), UTC, True),
+    ],
+)
 def test_validate_submission_date(submission_date, current_timezone, expected_validity):
     is_valid, reason = validate_submission_date(submission_date, current_timezone)
     assert is_valid is expected_validity, reason
