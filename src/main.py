3298791856b1806b917726885fce4ec78857af70
Zoltan Kohalmy <zoltankohalmy@gmail.com>
2020-07-20 10:49:55 +0200

    Reformat

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ import pytz
 from . import exceptions
 
 
-TIMEZONE_BUDAPEST = pytz.timezone('Europe/Budapest')
+TIMEZONE_BUDAPEST = pytz.timezone("Europe/Budapest")
 CURRENT_TIMEZONE = TIMEZONE_BUDAPEST
 WORKING_HOURS_START = dt.time(9)
 WORKING_HOURS_END = dt.time(17)
@@ -37,12 +37,12 @@ def validate_submission_date(submission_date: dt.datetime, current_timezone=CURR
 def _validate_working_day(submission_date: dt.datetime, current_timezone):
     # TODO: add timezone
     if submission_date.weekday() in [5, 6]:  # Monday is index 0
-        raise exceptions.NotWorkingDay(f'Submission date <{submission_date}> is not a working day')
+        raise exceptions.NotWorkingDay(f"Submission date <{submission_date}> is not a working day")
 
 
 def _validate_working_hours(submission_date: dt.datetime, current_timezone):
     if not (WORKING_HOURS_START <= submission_date.astimezone(current_timezone).time() < WORKING_HOURS_END):
-        raise exceptions.NotWorkingHours(f'Submission date <{submission_date}> falls outside of working hours')
+        raise exceptions.NotWorkingHours(f"Submission date <{submission_date}> falls outside of working hours")
 
 
 def get_resolution_date(submission_date: dt.datetime, current_timezone=CURRENT_TIMEZONE):
